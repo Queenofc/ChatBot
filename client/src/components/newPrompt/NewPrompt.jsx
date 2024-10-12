2ead9fdd359a31403a2a9cfa2c11a99b899c175c
Queenofc <152055731+Queenofc@users.noreply.github.com>
2024-10-12 16:07:47 +0530

    added an ai that accept text input

--- client/src/components/newPrompt/NewPrompt.jsx
+++ client/src/components/newPrompt/NewPrompt.jsx
@@ -2,20 +2,38 @@ import Upload from "../upload/Upload";
 import "./newPrompt.css";
 import { IKImage } from "imagekitio-react";
 import { useEffect, useRef, useState } from "react";
+import model from "../../lib/gemini";
+import Markdown from "react-markdown";
 
 const NewPrompt = () => {
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
 
   const [img,setImg] = useState({
     isLoading:false,
     error:"",
-    dbData:{}
+    dbData:{},
   })
 
   const endRef = useRef(null);
 
   useEffect(() => {
     endRef.current.scrollIntoView({ behavior: "smooth" });
-  }, []);
+  }, [ question, answer, img.dbData]);
+
+  const add = async (text) =>{
+    setQuestion(text)
+    const result = await model.generateContent(text);
+    const response = await result.response;
+    setAnswer(response.text())
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const text = e.target.text.value;
+    if (!text) return;
+    add(text, false);
+  };
 
   return (
     <>
@@ -29,11 +47,16 @@ const NewPrompt = () => {
           transformation={[{ width: 380 }]}
         />
       )}
+      {question && <div className="message user">{question}</div>}
+      {answer && 
+        <div className="message ">
+          <Markdown>{answer}</Markdown>
+        </div>}
       <div className="endChat" ref={endRef}></div>
-      <form className="newForm">
+      <form className="newForm" onSubmit={handleSubmit}>
         <Upload setImg={setImg}/>
         <input id="file" type="file" multiple={false} hidden />
-        <input type="text" placeholder="Ask Anything..." />
+        <input type="text" name="text" placeholder="Ask Anything..." />
         <button>
           <img src="/arrow.png" alt="" />
         </button>
