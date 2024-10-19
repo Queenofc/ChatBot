68b66f479b4747bc430e5d4ad18619bf2f39ac3e
Queenofc <152055731+Queenofc@users.noreply.github.com>
2024-10-19 22:26:37 +0530

    added storage of chat to db

--- client/src/routes/dashboardPage/dashboardPage.jsx
+++ client/src/routes/dashboardPage/dashboardPage.jsx
@@ -1,6 +1,8 @@
 import "./dashboardPage.css";
+import { useAuth } from "@clerk/clerk-react";
 
 const DashboardPage = () => {
+  const { userId } = useAuth();
   const handleSubmit = async (e) => {
     e.preventDefault();
     const text = e.target.text.value;
@@ -8,6 +10,7 @@ const DashboardPage = () => {
 
     await fetch("http://localhost:3000/api/chats", {
       method: "POST",
+      credentials: "include",
       headers: {
         "Content-Type": "application/json",
       },
