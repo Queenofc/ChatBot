37702fbf0151fdada5f4db8e80ca5abad6016811
Queenofc <152055731+Queenofc@users.noreply.github.com>
2024-10-31 16:11:57 +0530

    single page chat functional

--- client/src/layouts/rootLayout/RootLayout.jsx
+++ client/src/layouts/rootLayout/RootLayout.jsx
@@ -1,5 +1,6 @@
 import "./rootLayout.css";
 import { Link, Outlet } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ClerkProvider, SignedIn, UserButton } from "@clerk/clerk-react";
 
 // Import your publishable key
@@ -8,10 +9,12 @@ const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key");
 }
+const queryClient = new QueryClient() 
 
 const RootLayout = () => {
   return (
     <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <QueryClientProvider client={queryClient}>
       <div className="rootLayout">
         <header>
           <Link to="/" className="logo">
@@ -28,6 +31,7 @@ const RootLayout = () => {
           <Outlet />
         </main>
       </div>
+      </QueryClientProvider>
     </ClerkProvider>
   );
 };
--- client/src/routes/chatPage/chatPage.jsx
+++ client/src/routes/chatPage/chatPage.jsx
@@ -1,32 +1,50 @@
+import { useQuery } from "@tanstack/react-query";
 import NewPrompt from "../../components/newPrompt/newPrompt";
 import "./chatPage.css";
+import { useLocation } from "react-router-dom";
+import Markdown from "react-markdown";
 
 const ChatPage = () => {
+
+  const path = useLocation().pathname;
+  const chatId = path.split("/").pop();
+  
+  const { isPending, error, data } = useQuery({
+    queryKey: ["chat", chatId],
+    queryFn: () =>
+      fetch(`${import.meta.env.VITE_API_URL}/api/chats/${chatId}`, {
+        credentials: "include",
+      }).then((res) => res.json()),
+  });
+
   return (
     <div className="chatPage">
       <div className="wrapper">
         <div className="chat">
-          <div className="message">Test message from ai</div>
-          <div className="message user">
-            Test message from user Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Aspernatur molestiae officia quae dolor, vel autem
-            tempora qui eveniet rerum. A perferendis illum porro numquam
-            suscipit dolores modi facilis soluta cupiditate.
-          </div>
-          <div className="message">Test message from ai</div>
-          <div className="message user">Test message from user</div>
-          <div className="message">Test message from ai</div>
-          <div className="message user">Test message from user</div>
-          <div className="message">Test message from ai</div>
-          <div className="message user">Test message from user</div>
-          <div className="message">Test message from ai</div>
-          <div className="message user">Test message from user</div>
-          <div className="message">Test message from ai</div>
-          <div className="message user">Test message from user</div>
-          <div className="message">Test message from ai</div>
-          <div className="message user">Test message from user</div>
-          <div className="message">Test message from ai</div>
-          <div className="message user">Test message from user</div>
+        {isPending
+            ? "Loading..."
+            : error
+            ? "Something went wrong!"
+            : data?.history?.map((message, i) => (
+              <>
+              {message.img && (
+                    <IKImage
+                      urlEndpoint={import.meta.env.VITE_IMAGE_KIT_ENDPOINT}
+                      path={message.img}
+                      height="300"
+                      width="400"
+                      transformation={[{ height: 300, width: 400 }]}
+                      loading="lazy"
+                      lqip={{ active: true, quality: 20 }}
+                    />
+                  )}
+              <div className={
+                message.role === "user" ? "message user" : "message"
+              } key={i}>
+                <Markdown>{message.parts[0].text}</Markdown>
+              </div>
+              </>
+            ))}
           <NewPrompt />
         </div>
       </div>
